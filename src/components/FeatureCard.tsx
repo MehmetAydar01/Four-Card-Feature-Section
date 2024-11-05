3ab5d4e155563936a6e4e571a05cc6221ce4dbf0
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-11-06 00:46:28 +0300

    Card props type tanimlandi ve yapi tamamlandi

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,4 +1,35 @@
-function FeatureCard() {
-  return <div>FeatureCard</div>;
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from '@/components/ui/card';
+
+type FeatureCardProps = {
+  title: string;
+  text: string;
+  icon: string;
+  topLine: string;
+};
+
+function FeatureCard({ title, text, icon, topLine }: FeatureCardProps) {
+  return (
+    <Card className='w-80 sm:w-96 shadow-xl relative'>
+      {/* Top Line */}
+      <div
+        className={`absolute top-0 left-0 w-full border-l-8 border-l-transparent border-t-0 border-r-8 border-r-transparent border-b-[4px] ${topLine}`}
+      ></div>
+      <CardHeader className='p-4 sm:p-8'>
+        <CardTitle className='text-2xl text-primary mb-2'>{title}</CardTitle>
+        <CardDescription className='text-primary-foreground w-72'>
+          {text}
+        </CardDescription>
+      </CardHeader>
+      <CardFooter className='p-4 sm:p-8 justify-end'>
+        <img src={icon} alt={`icon${title}`} />
+      </CardFooter>
+    </Card>
+  );
 }
 export default FeatureCard;
