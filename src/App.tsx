5bf3119f2e6f414324d6568fedd9d99343ccbbb0
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-11-05 17:10:17 +0300

    proje baslangic dosyalari yuklendi

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header';
+import Cards from '@/components/Cards';
+
+function App() {
+  return (
+    <div className='flex items-center justify-center flex-col py-20'>
+      <Header />
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
