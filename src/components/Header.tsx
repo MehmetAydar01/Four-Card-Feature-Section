1a6a8c9549b2fd8fd6a0ccfecc6055ae7c8a75e0
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-11-05 17:11:17 +0300

    Header component olusturuldu

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div className='text-center'>
+      <h1 className='text-4xl'>Reliable, efficient delivery</h1>
+      <h2 className='text-4xl'>Powered by Technology</h2>
+      <p>
+        Our Artificial Intelligence powered tools use millions of project data
+        points to ensure that your project is successful
+      </p>
+    </div>
+  );
+}
+
+export default Header;
