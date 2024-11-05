96dc62f3a5cd68dfa2269ad95e0c80af29363664
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-11-05 18:57:21 +0300

    Header yapisi tamamlandi

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 function Header() {
   return (
-    <div className='text-center'>
-      <h1 className='text-4xl'>Reliable, efficient delivery</h1>
-      <h2 className='text-4xl'>Powered by Technology</h2>
-      <p>
+    <div className='text-center font-poppins max-w-xl opacity-95 mb-16'>
+      <h1 className='text-4xl font-extralight mb-4 text-primary'>
+        Reliable, efficient delivery
+      </h1>
+      <h2 className='text-4xl font-semibold text-primary mb-6'>
+        Powered by Technology
+      </h2>
+      <p className='text-primary-foreground font-normal'>
         Our Artificial Intelligence powered tools use millions of project data
         points to ensure that your project is successful
       </p>
